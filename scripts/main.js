774be2bb61b05494d24f112d55376d69a203f08e
illcom94 <mikekannard@hotmail.com>
2022-02-06 11:14:11 -0800

    Updated to v9

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import Portent from "./portent.js";
-import { applyTokenDamage, createDamageList } from "../../midi-qol/module/utils.js"
+import { applyTokenDamage, createDamageList } from "../../midi-qol/src/module/utils.js"
 
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -9,12 +9,33 @@ Hooks.once("setup", () => {
     setupActorClass();
     setupItemClass();
     
+    // Hooks.on("createMeasuredTemplate", async (args) => {
+//     	//alert("template created!");
+//     	//console.log(args);
+//     	if (typeof canvas.scene._activeAuraEffect !== 'undefined' && canvas.scene._activeAuraEffect != null) {
+//      		let template = canvas.templates.get(args.data._id);
+//      		let disposition = canvas.scene._activeAuraEffect[0].actor.token.disposition
+//  			let effects = canvas.scene._activeAuraEffect[0].item.effects
+//  			let templateEffectData = []
+//  			for (let effect of effects) {
+//  			   	let data = {data: duplicate(effect), parentActorId: false, parentActorLink: false, entityType: "template", entityId:template.id, casterDisposition: disposition, castLevel: canvas.scene._activeAuraEffect[0].spellLevel};
+//  				data.data.origin = `Actor.${canvas.scene._activeAuraEffect[0].actor._id}.Item.${canvas.scene._activeAuraEffect[0].item._id}`;
+//     				templateEffectData.push(data);
+//  			}
+//      		
+//      		await template.setFlag("ActiveAuras", "IsAura", templateEffectData);
+//  			AAhelpers.UserCollateAuras(canvas.scene._id, true, false, "spellCast");
+//  			canvas.scene._activeAuraEffect = null;
+//      	} 
+//      	return args;
+//     });
+    
     Hooks.on("midi-qol.AttackRollComplete", async (workflow) => {
     	for (let t of workflow.hitTargets) {
     		let a = t?.actor;
     		if (a != null) {
     			if (workflow.item.data.data.actionType == "mwak") {
-    				let aoa = a.data.effects.filter(ef => ef.label == "Armor of Agathys");
+    				let aoa = a.data.effects.filter(ef => ef.data.label == "Armor of Agathys");
     				if (aoa.length > 0 && a.data.data.attributes.hp.temp != null && a.data.data.attributes.hp.temp > 0) {
 	    				let armorFeat = a.items.filter(i => i.type == "feat" &&  i.name == 'Armor of Agathys Damage')[0];
 	    				let damage = parseInt(armorFeat.data.data.damage.parts[0][0]);
@@ -26,7 +47,7 @@ Hooks.once("setup", () => {
 	    				}
     					let dmg = await applyTokenDamage([ { damage: damage, type: type} ], damage, [ token ], armorFeat, new Set());
     					workflow.actor.applyDamage(dmg[0].hpDamage, 1);
-    					let messageContent = `<hr><div style="color: black; font-size: 1em;">${workflow.actor.name} takes ${dmg[0].hpDamage} from ${a.name}'s ${aoa[0].label}.</div><hr>`;
+    					let messageContent = `<hr><div style="color: black; font-size: 1em;">${workflow.actor.name} takes ${dmg[0].hpDamage} from ${a.name}'s ${aoa[0].data.label}.</div><hr>`;
     					ChatMessage.create({content: messageContent});
     				}
     			}
@@ -36,7 +57,7 @@ Hooks.once("setup", () => {
 });
 
 function setupActorClass() {
-	libWrapper.register("dnd5e-kannard", "CONFIG.Actor.entityClass.prototype.longRest", 
+	libWrapper.register("dnd5e-kannard", "CONFIG.Actor.documentClass.prototype.longRest", 
 	async function(wrapped, args = {}) {
 		console.log(args);
  	    let data = await wrapped(args);
@@ -53,7 +74,7 @@ function setupActorClass() {
 }
 
 function setupItemClass() {
-    libWrapper.register("dnd5e-kannard", "CONFIG.Item.entityClass.prototype.rollDamage", 
+    libWrapper.register("dnd5e-kannard", "CONFIG.Item.documentClass.prototype.rollDamage", 
     async function(wrapped, args = {}) //{ event = null, spellLevel = null, versatile = null } = {}) 
     {     	
 	    let addedHex = false;
@@ -65,9 +86,9 @@ function setupItemClass() {
     		for (let t of game.user.targets) {
     			let a = t?.actor;
     			if (a != null) {
-    				let hexes = a.data.effects.filter(ef => ef.label == "Hex");
+    				let hexes = a.data.effects.filter(ef => ef.data.label == "Hex");
     				for (let hex of hexes) {
-    					if (hex.origin.split('.')[1] == this.actor.id) {
+    					if (hex.data.origin.split('.')[1] == this.actor.id) {
     						addedHex = true;
     						break;
     					}
@@ -90,7 +111,7 @@ function setupItemClass() {
 
     	let itemHasHex = itemData.damage.parts[partCount - 1].length == 3 && itemData.damage.parts[partCount - 1][2] == "Hex";
     	if (addedHex && !itemHasHex) {
-  			itemData.damage.parts.push(["1d6", "necrotic", "Hex"]);
+  			itemData.damage.parts.push(["1d6[necrotic]", "necrotic", "Hex"]);
   		}
     	if (!addedHex && itemHasHex) {
     		itemData.damage.parts.pop();
